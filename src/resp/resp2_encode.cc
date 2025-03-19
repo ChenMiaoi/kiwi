ceeb9434a313b1b595d404a209aee1b502afbe34
marsevilspirit <marsevilspirit@gmail.com>
2025-03-19 21:37:59 +0800

    fix

--- src/resp/resp2_encode.cc
+++ src/resp/resp2_encode.cc
@@ -17,7 +17,7 @@ void Resp2Encode::SetRes(CmdRes ret, const std::string& content) {
       SetLineString("+PONG");
       break;
     case CmdRes::kSyntaxErr:
-      SetLineString("-ERR syntax error");
+      AppendStringRaw(fmt::format("-ERR syntax error\r\n", content));
       break;
     case CmdRes::kUnknownCmd:
       AppendStringRaw(fmt::format("-ERR unknown command '{}'\r\n", content));
